f4c06b80df5bb4707c0f801172d94ddc2a8ba5b8
Thet <thetnyihmue12@gmail.com>
2023-05-27 03:20:34 +0630

    Resolve char reading & add functions

---
+++ command.h
@@ -0,0 +1,64 @@
+#define NUM_CMD 34
+
+int power(int base, unsigned int p)
+{
+    int temp = base;
+    if(p == 0)
+        return 1;
+    for(int i=1; i<p; i++)
+    {
+        temp *= base;
+    }
+    return temp;
+}
+
+const char* command_list[NUM_CMD] = 
+{
+    "NUL",
+    "SOH",
+    "STX",
+    "ETX",
+    "EOT",
+    "ENQ",
+    "ACK",
+    "BEL",
+    "BS",
+    "HT",
+    "LF",
+    "VT",
+    "FF",
+    "CR",
+    "SO",
+    "SI",
+    "DLE",
+    "DC1",
+    "DC2",
+    "DC3",
+    "DC4",
+    "NAK",
+    "SYN",
+    "ETB",
+    "CAN",
+    "EM",
+    "SUB",
+    "ESC",
+    "FS",
+    "GS",
+    "RS",
+    "US",
+    "SPACE",
+    "DEL"
+};
+
+/*
+ * Status
+ *
+ * 1 : ascii : Insuficient Arguments
+ * 2 : ascii ['char']
+ * 3 : ascii [flag]
+ * 4 : ascii [number]
+ * 5 : acsii ['char'] [flag]
+ * 6 : acsii ['char'] [flag] [flag]
+ * 7 : acsii ['char'] [flag] [flag] [flag]
+ * 8 : Wrong Arguments
+*/
--- main.c
+++ main.c
@@ -1,103 +1,249 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <string.h>
+#include "command.h"
 
-#define NOTFOUND 128
-#define DECIMAL 10
-#define BINARY 2
-#define HEXA 16
-#define ALL 500
+#define NOT_FOUND   128
+#define DECIMAL     10
+#define BINARY      11
+#define HEXA        12
+#define ALPHA       13
 
-int check_flag(const char* flag);
-char check_char(const char* ch);
-int DtoB(const char ch);
+int check_flag(const char* flag_str);
+unsigned char check_char(const char* char_str);
+unsigned char check_cmd(const char* cmd_str);
+int check_num(const char* num_str);
+void check_h_v(const char* str);
+void DtoB_print(const char ch);
+void print_Alpha(const char ch);
 void print(const char ch, const int flag);
+void print_chart(int flag);
 
 int main (const int argc, const char* argv[])
 {
     unsigned char c = 0;
-    int flag = 0;
-    switch (argc)
-    {   case 1: 
+    int c_num = 0;
+    int status = 1;
+    int flag_[3] = {0,0,0};
+
+    for(int i=1; i<argc; i++)
+    {
+        if(argc >5)
+        {
+            status = 8;
+            break;
+        }
+        if(argc == 2)
+            check_h_v(argv[1]);
+        if(i == 1)
+        {
+            c = check_char(argv[i]);
+            if (c != NOT_FOUND)
+            {
+                status = 2;
+                continue;
+            }
+            flag_[i-1] = check_flag(argv[i]);
+            if(flag_[i-1] != NOT_FOUND)
+                status = 3;
+            else
+            {
+                c_num = check_num(argv[i]);
+                status = (c_num == NOT_FOUND) ? 8 : 4;
+            }
+            
+            if(status != 2 && argc != 2)
+                status = 8;
+            
+            if((status == 3) || (status == 4) || (status == 8))
+                break;
+        }
+        flag_[i-2] = check_flag(argv[i]);
+        if(flag_[i-2] == NOT_FOUND)
+        {
+            status = 8;
+            break;
+        }
+        else
+            status = argc + 2;
+    }
+    
+    switch (status)
+    {    
+        case 1:
             printf("Insufficient Arguments\n");
             break;
-        case 2: 
-            flag = check_flag(argv[1]);
-            if(flag != NOTFOUND)
-            {
-                for(int i=0; i<128; i++)
-                {
-                    print((char)i,flag);
-                    i%10 == 0 ? printf("\n") : printf("\t");
-                }
-                break;
-            }
-            else 
-            {
-                c = check_char(argv[1]);
-                if(c == NOTFOUND)
-                {
-                    printf("Wrong Arguments\n");
-                    break;
-                }
-                print(c,ALL);
-                break;
-            }
-            printf("Wrong Arguments\n");
+        case 2:
+            print(c,ALPHA);
+            print(c,DECIMAL);
+            print(c,BINARY);
+            print(c,HEXA);
+            printf("\n");
             break;
         case 3:
-            c = check_char(argv[1]);
-            flag = check_flag(argv[2]);
-            if(c == NOTFOUND || flag == NOTFOUND)
-            {
-                printf("Wrong Arguments\n");
-                break;
-            }
-            print(c,flag);
+            print_chart(flag_[0]);
+            break;
+        case 4:
+            printf("ASCII character for %s : ",argv[1]);
+            print(c_num,ALPHA);
+            printf("\n");
+            break;
+        case 5:
+            print(c,ALPHA);
+            print(c,flag_[0]);
+            printf("\n");
+            break;
+        case 6:
+            print(c,ALPHA);
+            print(c,flag_[0]);
+            print(c,flag_[1]);
+            printf("\n");
+            break;
+        case 7:
+            print(c,ALPHA);
+            print(c,flag_[0]);
+            print(c,flag_[1]);
+            print(c,flag_[2]);
+            printf("\n");
             break;
         default:
-            printf("Too much Arguments\n");
+            printf("Wrong Arguments\n");
             break;
     }
     return 0;
 }
 
-int check_flag(const char* flag)
+int check_flag(const char* flag_str)
 {
-    if((!strcmp("-d",flag)) || (!strcmp("--decimal",flag)))
+    if((!strcmp("-d", flag_str)) || (!strcmp("--decimal", flag_str)))
         return DECIMAL;
-    else if((!strcmp("-b",flag)) || (!strcmp("--binary",flag)))
+    else if((!strcmp("-b", flag_str)) || (!strcmp("--binary", flag_str)))
         return BINARY;
-    else if((!strcmp("-x",flag)) || (!strcmp("--hexa",flag)))
+    else if((!strcmp("-x", flag_str)) || (!strcmp("--hexa", flag_str)))
         return HEXA;
     else
-        return NOTFOUND;
+        return NOT_FOUND;
 }
 
-char check_char(const char* ch)
+unsigned char check_char(const char* char_str)
 {
-    if(strlen(ch) != 3)
-        return NOTFOUND;
-    if((ch[0] != '\'') || (ch[2] != '\''))
-        return NOTFOUND;
-    if(ch[1] > 127)
-        return NOTFOUND;
-    return ch[1];
+    int str_length = strlen(char_str);
+    int cmd_length = str_length-1;
+    char cmd_str[cmd_length-1];
+
+    if((char_str[0] != '\'') || (char_str[str_length-1] != '\''))
+        return NOT_FOUND;
+    if(str_length == 3 && char_str[1] < 128)
+        return char_str[1];
+
+    for(int i=0; i<str_length-2; i++)
+    {
+        cmd_str[i] = char_str[i+1];
+        if(cmd_str[i] == '\'')
+            cmd_str[i] = '\0';
+    }
+    return check_cmd(cmd_str);
 }
 
-int DtoB(const char ch)
+unsigned char check_cmd(const char* cmd_str)
 {
-    int B = 0;
-    int B_array[8];
+    int i;
+    for(i=0; i<NUM_CMD; i++)
+    {
+        if(!strcmp(cmd_str,command_list[i]))
+            return (i == 33) ? 127 : i;
+    }
+    return NOT_FOUND;
+}
+
+int check_num(const char* num_str)
+{
+    int num = 0, length = 0, hex_temp = 0;
+    length = strlen(num_str);
+    if(num_str[1] == 'b' || num_str[1] == 'B')
+    {
+        if(length>9)
+            return NOT_FOUND;
+        for(int i=2; i<length; i++)
+        {
+            if(num_str[i] != '0' && num_str[i] != '1')
+                return NOT_FOUND;
+            num = (num << 1) + (num_str[i]-48);
+        }
+    }
+    else if(num_str[1] == 'x' || num_str[1] == 'X')
+    {
+        if(length>4)
+            return NOT_FOUND;
+        for(int i=2; i<length; i++)
+        {
+            if(num_str[i] < '0')
+                return NOT_FOUND;
+            if(num_str[i] > '9' && num_str[i] < 'A')
+                return NOT_FOUND;
+            if(num_str[i] > 'F' && num_str[i] < 'a')
+                return NOT_FOUND;
+            if(num_str[i] > 'f')
+                return NOT_FOUND;
+            hex_temp = (int)num_str[i]-48;
+            hex_temp = (num_str[i] >= 'A' && num_str[i] <= 'F') ? (int)num_str[i]-55 : hex_temp;
+            hex_temp = (num_str[i] >= 'a' && num_str[i] <= 'f') ? (int)num_str[i]-87 : hex_temp;
+            num = (i == 2) ? hex_temp<<4 : num + hex_temp;
+        }
+    }
+    else
+    {
+        for(int i=0;i<length;i++)
+        {   
+            if(num_str[i] < '0' || num_str[i] > '9')
+                return NOT_FOUND;
+            num += (int)(num_str[i]-48) * power(10,(length-i)-1);
+        }
+    }
+    return num;
+}
+
+void check_h_v(const char* str)
+{
+    if((!strcmp("-v", str)) || (!strcmp("--version", str)))
+    {
+        printf("ASCII Table\nVersion 1.0 (2023)\nFree Software by Thomas.\n");
+        exit(0);
+    }
+    else if((!strcmp("-h", str)) || (!strcmp("--help", str)))
+    {
+        printf("This will print help\n");
+        exit(0);//help fun will be added soon
+    }
+}
+
+void DtoB_print(const char ch)
+{
+    int B_arr[7];
     unsigned int c = (int) ch;
     for(int i=0; i<7; i++)
     {
-        B_array[i] = c%2;
+        B_arr[i] = c%2;
         c = c>>1;
-        i == 0 ? (B = B_array[i]) : (B = (B*10) + B_array[i]);
     }
-    return B;
+    printf(" 0b");
+    for(int i=6; i>=0; i--)
+    {
+        printf("%d",B_arr[i]);
+    }
+    printf("  ");
+}
+
+void print_Alpha(const char ch)
+{
+    if(ch < ' ')
+        printf("%-3s ",command_list[(int)ch]);
+    else if(ch == ' ')
+        printf("\' \' ");
+    else if(ch == 127)
+        printf("%-3s ",command_list[33]);
+    else
+        printf("%-3c ",ch);
 }
 
 void print(const char ch, const int flag)
@@ -105,16 +251,32 @@ void print(const char ch, const int flag)
     switch (flag)
     {
         case DECIMAL:
-            printf("%c (%u)",ch,(int)ch);
+            printf("%3u  ",(int)ch);
             break;
         case BINARY:
-            printf("%c (0b%d)",ch,DtoB(ch));
+            DtoB_print(ch);
             break;
         case HEXA:
-            printf("%c (0x%x)",ch,ch);
+            if(ch<16)
+                printf("0x0%x  ",ch);
+            else
+                printf("0x%x  ",ch);
             break;
-        default:
-            printf("%c (%u) (0b%d) (0x%x)",ch,(int)ch,DtoB(ch),ch);
+        case ALPHA:
+            print_Alpha(ch);
             break;
-    }    
+    }
+}
+
+void print_chart(int flag)
+{
+    for(int i=0; i<128; i++)
+    {
+        print(i,flag);
+        print_Alpha(i);
+        if(i%8 == 7)
+            printf("\n");
+        else
+            printf(" ");
+    }
 }
